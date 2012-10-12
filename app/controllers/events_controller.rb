5f4670429fe62f8a50c6221e0bc5142be0a93b60
Hugo Nomura <hugonomura@gmail.com>
2012-10-12 18:00:24 -0300

    MVC dos eventos feito

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,11 +21,28 @@ class EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+
+    @event = Event.find(params[:id])
+
+    respond_to do |format|
+      if @event.update_attributes(params[:event])
+        format.html { redirect_to @event, notice: 'Evento atualizado com sucesso'}
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+    
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
   end
 end
